e0fd86a1599de8c85012252c4100fff4a390f47e
joeperpetua <qwerty.perpetua@gmail.com>
2019-08-26 22:13:20 -0300

    Agrego el index.js con la sintaxis para mostrar productos, todavia no funciona

--- www/js/index.js
+++ www/js/index.js
@@ -1,14 +1,55 @@
 var req = new XMLHttpRequest();
 var url = 'http://localhost/e-commerce/api/producto.php';
-var respuesta=null;
 req.open("GET",url,true);
+var $lista = document.getElementById('prod');
 
-req.onreadystatechange = function(){
-    if(req.status==200 && req.readyState==4){
-        respuesta=JSON.parse(req.responseText);
-        console.log(respuesta);
-    }
-
+function traerLista(){
+    req.onreadystatechange = function(){
+       if(req.status==200 && req.readyState==4){
+            var respuesta = JSON.parse(req.responseText);
+            console.log(respuesta);
+            crearLista(respuesta);
+       }
+    };
+    req.send(null);
 }
-req.send(null);
+
+
+function crearLista(resp){
+    
+    var lista = '';
+    var $mostrar = document.getElementById('prod');
+    
+    resp.forEach(function(elemento, index){
+        lista += '<label>'+ elemento.id_product +'</label><br>'
+                +'<label>'+ elemento.disp_brand +'</label><br>' 
+                +'<label>'+ elemento.disp_model +'</label><br>';
+    });
+
+    $mostrar.innerHTML = lista; 
+}
+
+
+
+window.onload = function(){
+    traerLista();
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
 
