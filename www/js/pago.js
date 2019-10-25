d8168b1fc8dd5233a9e0756d5bccc52dc3bd5d5e
Joel Perpetua <qwerty.perpetua@gmail.com>
2019-10-25 11:27:21 -0300

    El form ahora inserta id_prod, cant , y total a la BD. La API manda mail de orden de compra

--- www/js/pago.js
+++ www/js/pago.js
@@ -29,23 +29,34 @@ function traerLista(){
 }
 
 
-
+var order_subtotal;
 
 function crearLista(resp){
     
     var lista = '';
     var $mostrar = document.getElementById('resumen');
-    
+    var order_cant;
     resp.forEach(function(elemento, index){
         id = elemento.id_product;
+        order_subtotal = elemento.product_price;
+
+        order_cant = document.createElement('input');
+        order_cant.setAttribute('type', 'number');
+        order_cant.setAttribute('min', '1');
+        order_cant.setAttribute('value', '1');
+        order_cant.setAttribute('id', 'order_cant');
+        var cant = document.createTextNode(elemento.product_price);
+        order_cant.appendChild(cant);
+        
+        
         lista += '<img src="' + elemento.disp_pic + '" width="100px" alt="miniatura de '+ elemento.cpu_brand + ' ' + elemento.cpu_model +'" >' 
                         +'<h4>'+ elemento.disp_brand + ' ' + elemento.disp_model + '</h4>'
-                   +'<h1>$'+ elemento.product_price +'</h1>'
-                    +'<input type="number" placeholder="cantidad">';
+                        + '<h1>' + elemento.product_price + '<h1>';
+        
     });
 
     $mostrar.innerHTML = lista; 
-    
+    $mostrar.appendChild(order_cant);
 }
 
 
@@ -73,24 +84,33 @@ function menu() {
 
 //POST
 // formDos
+
+
 let $formulario = document.querySelector('#formulario'),
     $order_name = document.querySelector('#formulario #order_name'),
     $order_surname = document.querySelector('#formulario #order_surname'),
     $order_tel = document.querySelector('#formulario #order_tel'),
     $order_mail = document.querySelector('#formulario #order_mail'),
     $formMsg = document.querySelector('#formulario .formMsg');
+    
+    
 
     $formulario.onsubmit = function(evento){
     evento.preventDefault();
+    
     let formularioEsValido = false;
 
     let parametros = {
         order_name: $order_name.value,
         order_surname: $order_surname.value,
         order_tel: $order_tel.value,
-        order_mail: $order_mail.value
+        order_mail: $order_mail.value,
+        order_subtotal: parseInt(order_subtotal),
+        order_cant: parseInt(order_cant.value),
+        id_prod: id
     };
-
+    console.log(parametros);
+    
     formularioEsValido = parametros.order_name && parametros.order_surname && parametros.order_tel && parametros.order_mail;
 
     if (formularioEsValido)
