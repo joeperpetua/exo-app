e1e63c7529fb972d363462571ad75d404563fcd6
Joel Perpetua <qwerty.perpetua@gmail.com>
2019-10-24 22:17:39 -0300

    Envia form a BD con POST mediante API

--- www/js/pago.js
+++ www/js/pago.js
@@ -2,14 +2,21 @@ window.onload = function(){
     traerLista();
 };
 
-
+//GET
 var req = new XMLHttpRequest();
-var url = 'http://localhost/e-commerce/api/producto.php'+window.location.search;
-
-   
+var url = 'http://exophone.000webhostapp.com/api/producto.php'+window.location.search;
 req.open("GET",url,true);
 
+//POST
+// var reqPOST = new XMLHttpRequest();
+// var urlPOST = 'http://exophone.000webhostapp.com/api/producto.php';
+// var params = '&name=';
+// reqPOST.open("POST",urlPOST,true);
+// reqPOST.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 
+
+
+//GET
 function traerLista(){
     req.onreadystatechange = function(){
        if(req.status==200 && req.readyState==4){
@@ -20,6 +27,9 @@ function traerLista(){
     };
     req.send(null);
 }
+
+
+
 
 function crearLista(resp){
     
@@ -38,10 +48,7 @@ function crearLista(resp){
     
 }
 
-function compra(){
-    alert("Compra realizada");
-    window.open("index.html");
-}
+
 
 function menu() {
     var $button = document.getElementById('menuButton');
@@ -54,3 +61,64 @@ function menu() {
         document.getElementById('navbarCollapse').className = 'collapse';
     }
 }
+
+
+
+
+
+
+
+
+
+
+//POST
+// formDos
+let $formulario = document.querySelector('#formulario'),
+    $order_name = document.querySelector('#formulario #order_name'),
+    $order_surname = document.querySelector('#formulario #order_surname'),
+    $order_tel = document.querySelector('#formulario #order_tel'),
+    $order_mail = document.querySelector('#formulario #order_mail'),
+    $formMsg = document.querySelector('#formulario .formMsg');
+
+    $formulario.onsubmit = function(evento){
+    evento.preventDefault();
+    let formularioEsValido = false;
+
+    let parametros = {
+        order_name: $order_name.value,
+        order_surname: $order_surname.value,
+        order_tel: $order_tel.value,
+        order_mail: $order_mail.value
+    };
+
+    formularioEsValido = parametros.order_name && parametros.order_surname && parametros.order_tel && parametros.order_mail;
+
+    if (formularioEsValido)
+    {
+        let request = new XMLHttpRequest();
+        let url = 'http://exophone.000webhostapp.com/api/producto.php';
+        request.open('POST', url, true);// configuro mi request para que sea tipo POST
+        // Este header es necesario para comunicar al servidor dónde son enviados los parámetros
+        // application/json : los parámetros se enviaran a través del body del request
+        request.setRequestHeader("Content-Type", "application/json");
+        request.onreadystatechange = function(){
+            if(request.status==200 && request.readyState==4){
+                    let respuestaDelServidor = JSON.parse(request.responseText);
+                    if (respuestaDelServidor.response === 'ok')
+                    {
+                        $formMsg.innerText = 'El producto ha sido comprado';
+                        $formulario.reset();
+                    } else {
+                        $formMsg.innerText = 'Verificar los datos ingresados ' + request.status + ' / ' + request.readyState + ' / ' + request.responseText;
+                    }
+                    
+            }
+        };
+        // la función JSON.stringify() transforma una variable tipo object a un string con formato Json
+        request.send(JSON.stringify(parametros));
+    } else {
+        alert('complete los datos faltantes');
+    }
+
+};
+// fin formDos
